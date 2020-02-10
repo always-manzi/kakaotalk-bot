a1f4e450e6950d2aadafd8d310a1ed6876c565b9
manzi_hi <mangang9910@naver.com>
2020-02-10 23:38:55 +0900

    Update 매개변수.js

--- JavaScript/매개변수.js
+++ JavaScript/매개변수.js
@@ -4,5 +4,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
     replier.reply("채팅 내용 : " + msg); //채팅 내용
     replier.reply("보낸 사람 : " + sender); //보낸 사람
     replier.reply("단체 채팅방 : " + isGroupChat); //단톡방 여부
-    
+
   } 
+
+  
